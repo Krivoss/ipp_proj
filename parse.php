15151a21e91cbd21aceb43742b0dc95c8deec542
Krivoss <jakub.kriv@gmail.com>
2022-02-23 12:56:35 +0100

    started doing switch for instructions

--- parse.php
+++ parse.php
@@ -14,7 +14,7 @@ function arg_check() {
         switch ($option) {
             case 'h':
             case 'help':
-                echo("Usage: parser.php [options] <inputFile\n");
+                echo("Usage: php8.1 parser.php [options] <inputFile\n");
                 exit(0);
                 break;
             default:
@@ -26,13 +26,40 @@ function arg_check() {
 }
 
 function read_input() {
-    while($line = fgets(STDIN)) {
-        $line = remove_comments(trim($line, "\n"));    
-        $split = explode(' ', trim($line, " "));
-        foreach($split as $part) {
-            echo($part)."\n";
+    $header = false;
+    $programXML = new SimpleXMLElement("<program></program>");
+    while($line = fgets(STDIN)) {   
+        $line = remove_comments(trim($line, "\n"));
+        $line = trim($line, " ");
+        $split = explode(' ', $line);
+
+        if(!$header) {
+            if($line == ".IPPcode22") {
+                $header = true;
+                $programXML->addAttribute('language', $line);
+                
+
+            }
+        }
+
+        switch(strtoupper($split[0])) {
+            case 'DEFVAR':
+                $instruction = $programXML->addChild('instruction');
+                $instruction->addAttribute('opcode', $split[0]);
+                if(preg_match(("/(LF|GF|TF)@[a-zA-Z#$&*][a-zA-Z#$&*0-9]*/"), $split[1])) {
+                    $instructionArg1 = $instruction->addChild('arg1', $split[1]);
+                    $instructionArg1->addAttribute('type', 'var');
+                }
+                else {
+                    fwrite(STDERR, "WRONG DEFVAR OPERANDS\n");
+                }
         }
     }
+    // formating 
+    $doc = new DOMDocument();
+    $doc->loadXML($programXML->asXML());
+    $doc->formatOutput = true;
+    echo $doc->saveXML();
 }
 
 function remove_comments($line) {
