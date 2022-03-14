a19cc47e46aac3447cac4994b5195b284ee78de1
Krivoss <jakub.kriv@gmail.com>
2022-03-14 12:58:13 +0100

    Optimazed tests, added comments to parse and seperated some things to functions

--- test.php
+++ test.php
@@ -9,6 +9,7 @@
 arg_check();
 
 parser_test();
+
 //                        FUNCTIONS
 
 function arg_check() {
@@ -40,8 +41,6 @@ function arg_check() {
     }
 }
 
-
-
 function parser_test() {
     $src_files = get_src_files();
     $passed = 0;
@@ -57,15 +56,24 @@ function parser_test() {
         exec($command, $out, $parse_ret);
 
         $expected_rc = file_get_contents($file.".rc");
-
-        $diff_command = "java -jar jexamxml.jar temp.out ".$file.".out /dev/null /D options";
-        exec($diff_command, $out, $compare_ret);
-        if($parse_ret != $expected_rc || ($parse_ret == 0 && $compare_ret != 0)) {
+        
+        if($parse_ret != $expected_rc) {
             $failed++;
             array_push($failed_tests, $file);
         }
-        else { 
+        elseif ($parse_ret != 0) {
             $passed++;
+        }
+        else {            
+            $diff_command = "java -jar jexamxml.jar temp.out ".$file.".out /dev/null options";
+            exec($diff_command, $out, $compare_ret);
+            if($compare_ret != 0) {
+                $failed++;
+                array_push($failed_tests, $file);
+            }
+            else {
+                $passed++;
+            }
         }
     }
     echo("TEST RESULTS:\n\t\033[01;32mPASSED: ".$passed."\033[0m\n\t\033[01;31mFAILED: ".$failed."\033[0m\n");
@@ -80,16 +88,12 @@ function parser_test() {
 function get_src_files() {
     $o_dir = new RecursiveDirectoryIterator('./GHtests/parse-only');
     $o_iter = new RecursiveIteratorIterator($o_dir);
-
     $result = [];
-
     foreach ($o_iter as $o_info) {
         if ($o_info->getExtension() == 'src') {
             array_push($result , $o_info->getPathname());
         }
-    }
-    
+    }    
     return $result;
 }
-
 ?>
